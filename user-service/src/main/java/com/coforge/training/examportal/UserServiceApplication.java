1581d50ff7ce15fb5776a6ed5e03efe2792e8050
Suji M <Manthri.Sujitha@coforge.com>
2024-12-13 10:37:23 +0530

    Springboot files of the project

---
+++ user-service/src/main/java/com/coforge/training/examportal/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coforge.training.examportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
